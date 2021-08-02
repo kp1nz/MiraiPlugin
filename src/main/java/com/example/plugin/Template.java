1aced8d4309af2886cf115258edac64dd4bc7fdf
kp1nz <815153150@qq.com>
2021-08-02 14:05:31 +0800

    cus

--- src/main/java/com/example/plugin/Template.java
+++ src/main/java/com/example/plugin/Template.java
@@ -6,15 +6,23 @@ import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.contact.Group;
 import net.mamoe.mirai.event.GlobalEventChannel;
-import net.mamoe.mirai.event.events.FriendAddEvent;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
-import net.mamoe.mirai.event.events.NewFriendRequestEvent;
-import net.mamoe.mirai.message.data.*;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Template extends JavaPlugin {
     public static Template INSTANCE = new Template();
+    private static boolean status = false;
 
     private Template() {
         super(new JvmPluginDescriptionBuilder(
@@ -30,23 +38,55 @@ public class Template extends JavaPlugin {
 
     @Override
     public void onLoad(@NotNull PluginComponentStorage $this$onLoad) {
-
     }
 
     @Override
     public void onEnable() {
-        GlobalEventChannel.INSTANCE.subscribeAlways(NewFriendRequestEvent.class, NewFriendRequestEvent::accept);
-        GlobalEventChannel.INSTANCE.subscribeAlways(FriendAddEvent.class, (FriendAddEvent event) ->
-                event.getFriend().sendMessage("🐱"));
-        GlobalEventChannel.INSTANCE.subscribeAlways(FriendMessageEvent.class, (FriendMessageEvent event) -> {
-            //redirect message
-            Group group = Bot.getInstance(3634917467L).getGroup(1063117299);
-            event.getMessage().stream().filter(message -> message instanceof PttMessage).forEach(mes->
-                    group.sendMessage(mes));
-            group.sendMessage(event.getMessage());
-            event.getSender().sendMessage("喵");
-
+        GlobalEventChannel.INSTANCE.subscribeAlways(FriendMessageEvent.class, (FriendMessageEvent e) -> {
+            if (e.getSender().getId() == 815153150L) {
+                if (status) {
+                    e.getSender().sendMessage("already start");
+                } else {
+                    status = true;
+                    sendThread(e);
+                    e.getSender().sendMessage("start...");
+                }
+            }
         });
+//        AtomicInteger tag = new AtomicInteger();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        Friend friend = Bot.getInstance(3634917467L).getFriend(815153150L);
+//        try {
+//            while (true) {
+//                GlobalEventChannel.INSTANCE.subscribeOnce(FriendMessageEvent.class,(FriendMessageEvent e)->{
+//                    if (e.getSender().getId()==815153150L)
+//                        tag.set(1);
+//                });
+//                if (Integer.parseInt(loadRedis(0)) > 0) {
+////                    friend.sendMessage(loadRedis(1));
+//                }else {
+//                    loadRedis(1);
+//                }
+//                if (tag.equals(1)) break;
+//                Thread.sleep(100);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        Group group = Bot.getInstance(3634917467L).getGroup(1063117299);
+
+//        GlobalEventChannel.INSTANCE.subscribeAlways(FriendMessageEvent.class, (FriendMessageEvent event) -> {
+//            event.getSender().sendMessage(event.getMessage());
+//            try {
+//                event.getSender().sendMessage(getMessages());
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        });
 //            event.getMessage().stream().filter(message -> message instanceof Image).forEach(mes->
 //                    Bot.getInstance(3634917467L).getGroup(1063117299).sendMessage(mes));
 //            event.getSender().sendMessage(event.getMessage());
@@ -61,5 +101,86 @@ public class Template extends JavaPlugin {
 //                event.getSender().sendMessage("喵🐱");
 //            }
 //        });
+    }
+
+    private void sendThread(FriendMessageEvent e) {
+        Group group_jf = Bot.getInstance(3634917467L).getGroup(1067240772);
+        Group group_yj = Bot.getInstance(3634917467L).getGroup(528077046);
+        Group group_xwl = Bot.getInstance(3634917467L).getGroup(247856499);
+        //简放
+        Thread jf = new Thread(() -> {
+            while (true) {
+                try {
+                    if (status && !"null".equals(loadRedis(0, "jf")) && Integer.parseInt(loadRedis(0, "jf")) > 0) {
+                        group_jf.sendMessage(loadRedis(1, "jf"));
+                    }
+                    Thread.sleep(500);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+        //越甲
+        Thread yj = new Thread(() -> {
+            while (true) {
+                try {
+                    if (status && !"null".equals(loadRedis(0, "yj")) && Integer.parseInt(loadRedis(0, "yj")) > 0) {
+                        group_yj.sendMessage(loadRedis(1, "yj"));
+                    }
+                    Thread.sleep(500);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+        //徐闻亮
+        Thread xwl = new Thread(() -> {
+            while (true) {
+                try {
+                    if (status && !"null".equals(loadRedis(0, "xwl")) && Integer.parseInt(loadRedis(0, "xwl")) > 0) {
+                        group_xwl.sendMessage(loadRedis(1, "xwl"));
+                    }
+                    Thread.sleep(500);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+        jf.start();
+        yj.start();
+        xwl.start();
+    }
+
+    /**
+     * @param way 0 = getLength
+     */
+    public String loadRedis(int way, String key) throws MalformedURLException {
+        String len = null, mes = null;
+        try {
+            //动态载入指定类
+            File file = new File("/Users/kp1nz/Downloads/jedis-2.9.0.jar");//类路径(包文件上一层)
+            URL url = file.toURI().toURL();
+            ClassLoader loader = new URLClassLoader(new URL[]{url});//创建类载入器
+            Class<?> cls = loader.loadClass("redis.clients.jedis.Jedis");//载入指定类。注意一定要带上类的包名
+            Constructor<?> constructor = cls.getConstructor(String.class, int.class);
+            Object o = constructor.newInstance("127.0.0.1", 9736);
+            Method auth = cls.getMethod("auth", String.class);//方法名和相应的參数类型
+            Object jedis = auth.invoke(o, "90ed6019-cd3b-4cc7-998f-f65fe1c611ec");//调用得到的上边的方法method
+            if (way == 0) {
+                Method llen = cls.getMethod("llen", String.class);
+                len = String.valueOf(llen.invoke(o, key));
+            } else {
+                Method pop = cls.getMethod("lpop", String.class);
+                mes = String.valueOf(pop.invoke(o, key));
+            }
+        } catch (MalformedURLException | ClassNotFoundException | NoSuchMethodException | InstantiationException |
+                IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        if (way == 0) {
+            return len;
+        } else {
+            return mes;
+        }
     }
 }
